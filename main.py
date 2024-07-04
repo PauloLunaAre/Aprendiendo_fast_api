bddd4b7c2867b1277419ba366b3e79813bb3ea2e
PauloLunaAre <pluna_97@hotmail.com>
2024-07-04 10:09:38 -0600

    feat: Migracion a los modelos con alembic

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import uvicorn
 from app.routers import user
 from app.db.database import Base, engine
 
-def create_table():
-    Base.metadata.create_all(bind=engine)
-
-create_table()
 app = FastAPI()
 app.include_router(user.router)
 
